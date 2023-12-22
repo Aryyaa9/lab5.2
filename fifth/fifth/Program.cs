8030bc11c3d3e173940c718884cce12e0e538850
Aryyaa9 <153473157+Aryyaa9@users.noreply.github.com>
2023-12-22 18:34:03 +0700

    Add files via upload

---
+++ fifth/fifth/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+namespace ConsoleApp
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            string path = "../../../numsTask5.txt";
+
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string str = reader.ReadToEnd();
+
+                int[] nums = str.Split(' ').Select(int.Parse).ToArray();
+
+                int max = nums[0], min = nums[0];
+                int minNum = 0, maxNum = 0;
+
+                for (int i = 1; i < nums.Length; i++)
+                {
+                    if (nums[i] > max)
+                    {
+                        max = nums[i];
+                        maxNum = i;
+                    }
+
+                    if (nums[i] < min)
+                    {
+                        min = nums[i];
+                        minNum = i;
+                    }
+                }
+
+                int sum = 0;
+
+                for (int i = minNum + 1; i < maxNum; i++)
+                {
+                    sum += nums[i];
+                }
+
+                Console.WriteLine("Average: " + (double)sum / (double)(maxNum - minNum - 1));
+            }
+        }
+    }
+}
---
+++ fourth/fourth/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+namespace ConsoleApp
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            string path = "../../../numsTask4.txt";
+
+            using (StreamReader reader = new StreamReader(path))
+            {
+                string str = reader.ReadToEnd();
+
+                int[] nums = str.Split(' ').Select(int.Parse).ToArray();
+
+                int max = nums.Max();
+
+                int sum = 0;
+                for (int i = 0; i < nums.Length; i++)
+                {
+                    if (nums[i] == max - 1)
+                        sum += nums[i];
+                }
+
+                Console.WriteLine(sum);
+            }
+        }
+    }
+}
